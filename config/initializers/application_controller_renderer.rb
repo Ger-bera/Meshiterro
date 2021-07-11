81159118f74996587c2e535fe9f8ffb40ae5de2c
Ger-bera <1925dart@gmail.com>
2021-07-11 11:23:00 +0000

    [Start]4-5

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'fe63af75faaaaab33df7a6ad0b545eb8ff9d521960b7b859766b124ee1ebc2691877166540ef991534516de5d128f65e697cf839cc8baa826c2b55d1a905fd3b'
